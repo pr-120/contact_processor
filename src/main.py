98ea17490803cc8c187fb6683d4c6a97e5532281
pr-120 <patrikrosenkranz@gmail.com>
2024-11-12 14:21:21 +0100

    adjusted main for currently needed functionality

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-from src.query_contacts import delete_contacts
-from src.query_emails import get_invalid_mail_addresses
+from contact_processor.src.query_contacts import get_contacts_from_contact_group, add_canton_info_to_contact, \
+    modify_contact_groups
 
 
 def main():
-    invalid_addresses = get_invalid_mail_addresses()
-    delete_contacts(invalid_addresses)
+    # invalid_addresses = get_invalid_mail_addresses()
+    # delete_contacts(invalid_addresses)
+    people_in_contact_group = get_contacts_from_contact_group("Email_Gemeinden2025")
+    canton_dict = add_canton_info_to_contact(people_in_contact_group)
+    modify_contact_groups(canton_dict)
 
 
 if __name__ == "__main__":
